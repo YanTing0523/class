ce8d94bff56d4c260146a75ee4561a3de39d65f4
YanTing0523 <C109110140@nkust.edu.tw>
2023-05-01 17:00:38 +0800

    second commit

---
+++ 0313a.py
@@ -0,0 +1,4 @@
+f = open("hello.dat", "w", encoding="utf-8")
+for i in range(5):
+    f.write("你好\n")
+f.close()
---
+++ nkust1112web78/mysite/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+class HBicycleData(models.Model):
+    sna = models.CharField(max_length=100)
+    sbi = models.IntegerField(default=0)
+    tot = models.IntegerField(default=0)
+    class Meta:
+        ordering = ('-sbi',)
+    def __str__(self):
+        return self.sna
+
+class NKUSTnews(models.Model):
+    title = models.CharField(max_length=200)
+    def __str__(self):
+        return self.title
+    
+class PhoneMaker(models.Model):
+    name = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class PhoneModel(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    maker = models.ForeignKey(PhoneMaker, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["-price"]
